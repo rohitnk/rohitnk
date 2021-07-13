4f7dd2a593b0c7867e22929991034a1b32ac54fb
Rohit Kalpavruksha <rohitkalpavruksha@gmail.com>
2021-07-13 22:59:07 +0530

    hahaha

--- Questions/question.js
+++ Questions/question.js
@@ -1,30 +1,39 @@
 import q from "./que.js";
 // console.log(q);
+let questionNo = 0;
 function createquestion(question_statement, option, answer) {
+  questionNo++;
+  answer += 1;
   const app = document.getElementById("app");
   const div = document.createElement("div");
   app.appendChild(div);
   const questionStatement = document.createElement("h3");
+  questionStatement.setAttribute("class", "question");
   div.appendChild(questionStatement);
-  questionStatement.textContent = question_statement;
-  var count = -1;
+  questionStatement.textContent = "Q." + questionNo + " " + question_statement;
+  var count = 0;
   for (const op of option) {
     count++;
     const opdiv = document.createElement("div");
     console.log(count, answer);
     div.appendChild(opdiv);
-    opdiv.textContent = op;
+    opdiv.setAttribute("class", "option");
+    opdiv.textContent = count + ". " + op;
     if (count === answer)
       opdiv.addEventListener("click", function () {
-        alert("This is correct option");
+        opdiv.style.backgroundColor = "green";
+        opdiv.style.color = "white";
+        // alert("This is correct option");
       });
     else
       opdiv.addEventListener("click", function () {
-        alert("This is Wrong option");
+        opdiv.style.backgroundColor = "red";
+        opdiv.style.color = "white";
+        // alert("This is Wrong option");
       });
   }
 }
-
+debugger;
 for (const options of q) {
   createquestion(options.question, options.answers, options.correctIndex);
 }
