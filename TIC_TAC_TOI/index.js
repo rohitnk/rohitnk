6c133dd55a613156ea745a860938864fa30c5260
Rohit Kalpavruksha <rohitkalpavruksha@gmail.com>
2021-07-20 17:25:56 +0530

    v1.1

--- TIC_TAC_TOI/index.js
+++ TIC_TAC_TOI/index.js
@@ -1,4 +1,7 @@
 const table = document.getElementById("table");
+let m = document.getElementById("matches");
+let matches = m.value;
+console.log(matches);
 for (let i = 1; i < 4; i++) {
   const tr = document.createElement("tr");
   tr.setAttribute("id", i);
@@ -6,57 +9,109 @@ for (let i = 1; i < 4; i++) {
   for (let j = 1; j < 4; j++) {
     td = document.createElement("td");
     td.setAttribute("id", `${i}${j}`);
-    // tdt.set ("id", i);
     tr.appendChild(td);
   }
 }
-let player1 = true;
-let player2 = false;
 
-let p1 = {
-  11: false,
-  12: false,
-  13: false,
-  21: false,
-  22: false,
-  23: false,
-  31: false,
-  32: false,
-  33: false,
-};
+function MatchReset() {
+  player1 = true;
+  player2 = false;
 
-let p2 = {
-  11: false,
-  12: false,
-  13: false,
-  21: false,
-  22: false,
-  23: false,
-  31: false,
-  32: false,
-  33: false,
-};
+  p1 = {
+    11: false,
+    12: false,
+    13: false,
+    21: false,
+    22: false,
+    23: false,
+    31: false,
+    32: false,
+    33: false,
+  };
+
+  p2 = {
+    11: false,
+    12: false,
+    13: false,
+    21: false,
+    22: false,
+    23: false,
+    31: false,
+    32: false,
+    33: false,
+  };
+  count = 0;
+  gameStart = false;
+
+  for (let i = 1; i < 4; i++) {
+    for (let j = 1; j < 4; j++) {
+      td = document.getElementById(`${i}${j}`);
+      td.style.background = "white";
+    }
+  }
+  m.value = "";
+}
+function masterReset() {
+  MatchReset();
+  m.disabled = false;
+}
+masterReset();
+let player1Win = 0;
+let player2Win = 0;
 
 function myFunction(event) {
+  let matches = m.value;
+  console.log(matches);
+  // debugger;
+  // console.log(`before click the count is ${count}`);
+  if (!gameStart) return;
+
   const clicked = event.target.id;
+  // console.log(clicked);
+  if (clicked == "table") return;
   const toChange = document.getElementById(`${clicked}`);
+  matches--;
+  m.value = matches;
   if (player1) {
     if (clickCheck(p1, clicked, p2)) return;
-    toChange.style.backgroundColor = "red";
-    if (winCheck(p1)) hurray("Player 1");
+    count++;
+    toChange.style.background =
+      "linear-gradient(90deg, rgba(255,142,142,1) 30%, rgba(183,4,4,1) 100%)";
+    if (winCheck(p1)) {
+      player1Win++;
+      hurray("Player 1");
+      gameStart = false;
+      return;
+    }
     player1 = false;
     player2 = true;
   } else {
     if (clickCheck(p2, clicked, p1)) return;
-    toChange.style.backgroundColor = "green";
-    if (winCheck(p2)) hurray("Player 2");
+    count++;
+    toChange.style.background =
+      "linear-gradient(90deg, rgba(213,247,164,1) 8%, rgba(0,255,38,1) 58%)";
+    if (winCheck(p2)) {
+      player2Win++;
+      hurray("Player 2");
+      gameStart = false;
+      return;
+    }
     player1 = true;
     player2 = false;
+  }
+  // console.log(`after click the count is ${count}`);
+  if (count === 9) {
+    setTimeout(function () {
+      alert("match draw");
+      reset();
+    }, 300);
+    gameStart = false;
+    return;
   }
 }
 
 function clickCheck(px, clicked, py) {
-  if (py[clicked]) return true;
+  if (py[clicked] || px[clicked]) return true;
   px[clicked] = true;
   return false;
 }
@@ -81,6 +136,12 @@ function winCheck(p) {
 
 function hurray(p) {
   setTimeout(function () {
-    alert(p);
-  }, 100);
+    alert(`${p} won `);
+    reset();
+  }, 300);
+}
+
+function play() {
+  gameStart = true;
+  m.disabled = true;
 }
