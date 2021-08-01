35fa799cfba8bfe153fc19e9a0706b782cae3765
Rohit <rohitkalpavruksha@gmail.com>
2021-08-01 11:39:19 +0530

    localStorage functanality added to retrive earlier lists

--- Todo List/index.js
+++ Todo List/index.js
@@ -3,35 +3,39 @@ const listContainer = document.getElementById("listContainer");
 var list = [];
 let mainList = [];
 
+function createTemplet(msg) {
+  const div = document.createElement("div");
+  div.setAttribute("class", "listItem");
+  div.setAttribute("id", `${count}`);
+  const p = document.createElement("p");
+  div.appendChild(p);
+  const btn = document.createElement("button");
+  btn.textContent = "Remove";
+  btn.setAttribute("onclick", `listItm.remove(${count})`);
+  const btnDiv = document.createElement("div");
+  btnDiv.appendChild(btn);
+  const editBtn = document.createElement("button");
+  editBtn.textContent = "Edit";
+  btnDiv.appendChild(editBtn);
+  div.appendChild(btnDiv);
+  p.textContent = msg;
+  listContainer.appendChild(div);
+  count++;
+}
+
 count = 0;
 class listItm {
-  constructor() {
+  constructor(msg) {
+    this.message = task.value;
     if (task.value == "") return;
-    const div = document.createElement("div");
-    div.setAttribute("class", "listItem");
-    div.setAttribute("id", `${count}`);
-    const p = document.createElement("p");
-    div.appendChild(p);
-    const btn = document.createElement("button");
-    btn.textContent = "Remove";
-    btn.setAttribute("onclick", `listItm.remove(${count})`);
-    const btnDiv = document.createElement("div");
-    btnDiv.appendChild(btn);
-    const editBtn = document.createElement("button");
-    editBtn.textContent = "Edit";
-    btnDiv.appendChild(editBtn);
-    div.appendChild(btnDiv);
-    console.log(task.value);
-    p.textContent = task.value;
-    listContainer.appendChild(div);
+    createTemplet(msg);
     task.value = "";
-    count++;
-    window.localStorage.setItem("mainList", mainList);
   }
 
   static remove(a) {
     list.pop(a);
     mainList.pop(a);
+    setToLocal();
     const toRemove = document.getElementById(a);
     toRemove.remove();
     console.log("removed");
@@ -41,12 +45,30 @@ class listItm {
 function addToList() {
   if (list.includes(task.value)) return;
   list.push(task.value);
-  a = new listItm();
+  a = new listItm(task.value);
   mainList.push(a);
-  var z = JSON.parse(window.localStorage.getItem("mainList"));
-  console.log(z[0]);
+  setToLocal();
 }
+
+if (localStorage.storedList) {
+  let stored_List = getFromLocal();
+  stored_List.forEach((element) => {
+    a = new listItm();
+    a.message = element.message;
+    mainList.push(a);
+    createTemplet(element.message);
+    list.push(element.message);
+  });
+}
+
 document.addEventListener("keyup", function (key) {
-  // console.log(key);
   if (key.key == "Enter") addToList();
 });
+
+function setToLocal() {
+  window.localStorage.setItem("storedList", JSON.stringify(mainList));
+}
+
+function getFromLocal() {
+  return JSON.parse(window.localStorage.getItem("storedList"));
+}
